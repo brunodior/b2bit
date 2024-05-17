88102a33fe26290b56a55a9a9ccdd7567d388da1
bruno dior <bruno.araujo.128@ufrn.edu.br>
2024-05-17 12:43:54 -0300

    fix: message color adjustment

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -7,21 +7,20 @@ function Message(){
     const [color, setColor] = useState('')
 
     useEffect(()=> {
-
         bus.addListener('flash', ({message, color}) =>{
             setVisibility(true)
-            setColor(color)
             setMessage(message)
+            console.log(color)
+            setColor(`bg-${color}-500`)
             setTimeout(() =>{
                 setVisibility(false)
             }, 3000)
         })
-
     }, [])
 
     return (
         visibility ? (
-                <div style={{zIndex: '1000'}} className={`rounded-xl shadow-lg absolute w-3/6 text-center mt-3 font-bold absolute translate-x-1/2 p-3 text-white border  bg-${color}-500`}>
+                <div style={{zIndex: '1000'}} className={`rounded-xl shadow-lg absolute w-3/6 text-center mt-3 font-bold absolute translate-x-1/2 p-3 text-white border  ${color}`}>
                    {message}
                 </div>
         ) : (
