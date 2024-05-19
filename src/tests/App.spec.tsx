467993b5c4b4fe04b8c58cf3293b80d51199bed1
bruno dior <bruno.araujo.128@ufrn.edu.br>
2024-05-19 10:35:50 -0300

    feat: testing doLogin function

--- src/tests/App.spec.tsx
+++ src/tests/App.spec.tsx
@@ -1,52 +1,72 @@
 import axios from 'axios';
-import { doLogin } from '../api/authService';
+import MockAdapter from 'axios-mock-adapter';
+import { doLogin } from '../api/authService'; 
 
-jest.mock('axios');
-const mockAxios = axios as jest.Mocked<typeof axios>;
 
-const mockResponse = {
-    data: {
-        "user": {
-            "id": 4,
-            "name": "Cliente",
-            "email": "cliente@youdrive.com",
-            "is_active": true,
-            "avatar": {
-                "id": 75,
-                "high": "https://you-drive-homologation-bucket.s3.amazonaws.com/media/images/30f2ade090db49d9a3dc7594a778ffa4_high.png",
-                "medium": "https://you-drive-homologation-bucket.s3.amazonaws.com/media/images/30f2ade090db49d9a3dc7594a778ffa4_medium.png",
-                "low": "https://you-drive-homologation-bucket.s3.amazonaws.com/media/images/30f2ade090db49d9a3dc7594a778ffa4_low.png"
-            },
-            "type": "StoreUser",
-            "created": "2023-09-20T11:42:54.515946-03:00",
-            "modified": "2024-05-17T18:06:24.635043-03:00",
-            "role": "OWNER"
-        },
-        "tokens": {
-            "refresh": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTc0NzUxNTk4OSwiaWF0IjoxNzE1OTc5OTg5LCJqdGkiOiJhZDM5NWM3NWI4NWI0ZmQ4ODI0MmJhNWI4MzZjZGNkMyIsInVzZXJfaWQiOjR9.plLoepQMSO3xwTvBYZrni5E91reLWvJgkuBcKpWRhi8",
-            "access": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE2MDY2Mzg5LCJpYXQiOjE3MTU5Nzk5ODksImp0aSI6IjI4NTYyYmJhZWM1OTQyZGU4ZjNhNGY5Y2M3NmI4OTY2IiwidXNlcl9pZCI6NH0.ZiZfu_ozjV_LX4B2gpeNdcljFCFUu5WUG-P33v5vbDY"
-        }
-    },
-    status: 200,
-    statusText: 'OK'
-}
 
-const mockUserData = {
-    "email": "cliente@youdrive.com",
-    "password": "password"
-}
 
-describe('Test API', () => {
-    afterEach(() => {
-        jest.resetAllMocks();
-    });
+describe('doLogin', () => {
+        let mock: MockAdapter;
 
-    test('should login successfully', async () => {
-        mockAxios.post.mockResolvedValueOnce(mockResponse);
+        beforeAll(() => {
+            mock = new MockAdapter(axios);
+        });
 
-        const data = await doLogin(mockUserData);
+        afterEach(() => {
+            mock.reset();
+        });
 
-        expect(mockAxios.post).toHaveBeenCalledTimes(1);
-        expect(data).toEqual(mockResponse.data); // Verifica a propriedade data da resposta
-    });
+        test('should successfully login with correct user credentials', async () => {
+            const mockUser = { email: 'cliente@youdrive.com', password: 'password' };
+            const mockResponse = {
+                data: {
+                user: {
+                    id: 4,
+                    name: 'Cliente',
+                    email: 'cliente@youdrive.com',
+                    is_active: true,
+                    avatar: {},
+                    type: 'StoreUser',
+                    created: '2023-09-20T11:42:54.515946-03:00',
+                    modified: '2024-05-17T18:06:24.635043-03:00',
+                    role: 'OWNER'
+                },
+                tokens: {
+                    refresh: 'fake-refresh-token',
+                    access: 'fake-access-token'
+                }
+                },
+                status: 200,
+                statusText: 'OK'
+            };
+
+            mock.onPost('https://api.homologation.cliqdrive.com.br/auth/login/').reply(200, mockResponse);
+
+            const response = await doLogin(mockUser);
+
+            expect(response.data.data).toEqual(mockResponse.data);
+            expect(response.status).toBe(200);
+        });
+
+        test('should fail login with incorrect user credentials', async () => {
+            const mockUserWrong = { username: 'wrong', password: 'wrong' };
+            const mockErrorResponse = {
+            response: {
+                data: { details: 'Usuário e/ou senha incorreto(s)' },
+                status: 401
+            }
+            };
+
+            mock.onPost('https://api.homologation.cliqdrive.com.br/auth/login/').reply(401, mockErrorResponse.response.data);
+
+            try {
+            await doLogin(mockUserWrong);
+            } catch (error: any) {
+            expect(error.response.data).toEqual(mockErrorResponse.response.data);
+            expect(error.response.status).toBe(401);
+            }
+        });
+
 });
+
+
